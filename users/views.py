2d0a99c256f2846261931e857f999b6df8d6bf8d
Julya Shapaeva <shapaeva03@mail.ru>
2024-08-14 06:27:28 +0400

    Add PaymentListAPIView and filters to it

--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics
-from users.models import User
-from users.serializers import UserSerializer
+from rest_framework.filters import OrderingFilter
+from users.models import User, Payment
+from users.serializers import UserSerializer, PaymentSerializer
 
 
 class UserUpdateAPIView(generics.UpdateAPIView):
@@ -11,3 +13,12 @@ class UserUpdateAPIView(generics.UpdateAPIView):
 class UserListAPIView(generics.ListAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+
+
+class PaymentListAPIView(generics.ListAPIView):
+    serializer_class = PaymentSerializer
+    queryset = Payment.objects.all()
+
+    filter_backends = [OrderingFilter, DjangoFilterBackend]
+    ordering_fields = ['date']
+    filterset_fields = ('course', 'lesson', 'way',)
