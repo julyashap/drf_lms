ef2eaf1202684ed0b46d5c47c15f5e3f7ba1e051
Julya Shapaeva <shapaeva03@mail.ru>
2024-08-14 06:10:20 +0400

    Update CourseSerializer with lessons field

--- materials/serializers.py
+++ materials/serializers.py
@@ -2,18 +2,19 @@ from rest_framework import serializers
 from materials.models import Course, Lesson
 
 
+class LessonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = '__all__'
+
+
 class CourseSerializer(serializers.ModelSerializer):
     count_lessons = serializers.SerializerMethodField()
+    lessons = LessonSerializer(many=True, source='lesson_set.all')
 
     def get_count_lessons(self, obj):
         return obj.lesson_set.count()
 
     class Meta:
         model = Course
-        fields = ['name', 'description', 'count_lessons']
-
-
-class LessonSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Lesson
-        fields = '__all__'
+        fields = ['name', 'description', 'count_lessons', 'lessons']
