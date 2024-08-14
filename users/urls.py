904563063aa4ea4d6126974ef14f026bda2f48c7
Julya Shapaeva <shapaeva03@mail.ru>
2024-08-14 06:06:15 +0400

    Add ListAPIView for User model

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from users.apps import UsersConfig
-from users.views import UserUpdateAPIView
+from users.views import UserUpdateAPIView, UserListAPIView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
+    path('list/', UserListAPIView.as_view(), name='list_user'),
     path('update/<int:pk>', UserUpdateAPIView.as_view(), name='update_user'),
 ]
