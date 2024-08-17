974630b3120e37ef2f7730f60ab5fb948af4ec4e
Julya Shapaeva <shapaeva03@mail.ru>
2024-08-17 18:24:30 +0400

    Add owner field to Course and Lesson models

--- materials/models.py
+++ materials/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from config import settings
+
 NULLABLE = {'null': True, 'blank': True}
 
 
@@ -7,6 +9,8 @@ class Course(models.Model):
     name = models.CharField(max_length=100, verbose_name='название')
     preview = models.ImageField(upload_to='courses/', verbose_name='превью', **NULLABLE)
     description = models.TextField(verbose_name='описание')
+
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='владелец', **NULLABLE)
 
     def __str__(self):
         return f"{self.name}"
@@ -23,6 +27,7 @@ class Lesson(models.Model):
     video = models.URLField(max_length=200, verbose_name='ссылка на видео', **NULLABLE)
 
     course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='курс')
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='владелец', **NULLABLE)
 
     def __str__(self):
         return f"{self.name}"
