5ac75050b47d51fef2f90057771dfcd197ab0b9b
Julya Shapaeva <shapaeva03@mail.ru>
2024-08-17 17:39:52 +0400

    Add router for UserViewSet

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
+from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from users.apps import UsersConfig
-from users.views import UserUpdateAPIView, UserListAPIView, PaymentListAPIView
+from users.views import UserViewSet, PaymentListAPIView
 
 app_name = UsersConfig.name
 
-urlpatterns = [
-    path('list/', UserListAPIView.as_view(), name='list_user'),
-    path('update/<int:pk>', UserUpdateAPIView.as_view(), name='update_user'),
+router = routers.DefaultRouter()
+router.register('', UserViewSet, basename='courses')
 
+urlpatterns = [
     path('payments/', PaymentListAPIView.as_view(), name='list_payment'),
-]
+
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+] + router.urls
