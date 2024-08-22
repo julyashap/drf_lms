a28a859bc60dc171332aa255f36bf430f6503a59
Julya Shapaeva <shapaeva03@mail.ru>
2024-08-23 02:26:37 +0400

    Update list() and retrieve() methods with context data in serializers in CourseViewSet

--- materials/views.py
+++ materials/views.py
@@ -1,7 +1,10 @@
-from rest_framework import generics, viewsets
-from materials.models import Course, Lesson
+from rest_framework import generics, viewsets, status
+from rest_framework import views
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
+from materials.models import Course, Lesson, CourseSubscribe
 from materials.permissions import IsModerator, IsOwner
-from materials.serializers import CourseSerializer, LessonSerializer
+from materials.serializers import CourseSerializer, LessonSerializer, CourseSubscribeSerializer
 
 
 class CourseViewSet(viewsets.ModelViewSet):
@@ -25,7 +28,19 @@ class CourseViewSet(viewsets.ModelViewSet):
     def list(self, request, *args, **kwargs):
         if not self.request.user.groups.filter(name='moderators').exists():
             self.queryset = Course.objects.filter(owner=request.user)
-        return super().list(request, *args, **kwargs)
+
+        queryset = self.filter_queryset(self.get_queryset())
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True, context={'user': self.request.user})
+            return self.get_paginated_response(serializer.data)
+        serializer = self.get_serializer(queryset, many=True, context={'user': self.request.user})
+        return Response(serializer.data)
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, context={'user': self.request.user})
+        return Response(serializer.data)
 
 
 class LessonListAPIView(generics.ListAPIView):
@@ -64,3 +79,31 @@ class LessonDestroyAPIView(generics.DestroyAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
     permission_classes = [IsOwner]
+
+
+class CourseSubscribeManager(views.APIView):
+
+    def post(self, *args, **kwargs):
+        user = self.request.user
+
+        course_pk = self.request.data.get('course')
+        course = get_object_or_404(Course, pk=course_pk)
+
+        course_subscribe = CourseSubscribe.objects.filter(user=user, course=course)
+
+        if not course_subscribe:
+            new_course_subscribe = CourseSubscribe.objects.create(user=user, course=course)
+            new_course_subscribe.save()
+
+            course_subscribe_serializer = CourseSubscribeSerializer(new_course_subscribe)
+            response = Response(course_subscribe_serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            course_subscribe.delete()
+            response = Response({"message": "Подписка удалена!"})
+
+        return response
+
+
+class CourseSubscribeListAPIView(generics.ListAPIView):
+    serializer_class = CourseSubscribeSerializer
+    queryset = CourseSubscribe.objects.all()
