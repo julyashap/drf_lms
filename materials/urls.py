736d47dcbb8d77737d7815f1ca7ac5bf4e80992f
Julya Shapaeva <shapaeva03@mail.ru>
2024-08-23 02:25:43 +0400

    Add urlpatterns for CourseSubscribeManager and CourseSubscribeListAPIView

--- materials/urls.py
+++ materials/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from materials.apps import MaterialsConfig
 from rest_framework import routers
 from materials.views import CourseViewSet, LessonListAPIView, LessonRetrieveAPIView, LessonCreateAPIView, \
-    LessonUpdateAPIView, LessonDestroyAPIView
+    LessonUpdateAPIView, LessonDestroyAPIView, CourseSubscribeManager, CourseSubscribeListAPIView
 
 app_name = MaterialsConfig.name
 
@@ -15,4 +15,7 @@ urlpatterns = [
     path('lesson/create/', LessonCreateAPIView.as_view(), name='create_lesson'),
     path('lesson/update/<int:pk>/', LessonUpdateAPIView.as_view(), name='update_lesson'),
     path('lesson/destroy/<int:pk>/', LessonDestroyAPIView.as_view(), name='destroy_lesson'),
+
+    path('course-subscribe/management/', CourseSubscribeManager.as_view(), name='manage_course_subscribe'),
+    path('course-subscribe/list/', CourseSubscribeListAPIView.as_view(), name='list_course_subscribe'),
 ] + router.urls
