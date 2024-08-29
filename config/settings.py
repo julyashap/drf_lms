eb8f3e6c9a30882796b55dcdcf3e87c77034e434
Julya Shapaeva <shapaeva03@mail.ru>
2024-08-29 21:08:55 +0300

    Add HOST and PORT keys to DATABASES settings

--- config/settings.py
+++ config/settings.py
@@ -67,7 +67,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD')
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': int(os.getenv('DB_PORT'))
     }
 }
 
